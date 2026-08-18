89e129b6b4bf8bcc26c458c8a105cabd479efbac
Mohamed Elsayed <elmoosayed@gmail.com>
2026-08-18 20:20:45 +0300

    fix: MainActivityTest JUnit test discovery for Firebase Test Lab

--- android/app/src/androidTest/java/com/example/invoify/MainActivityTest.java
+++ android/app/src/androidTest/java/com/example/invoify/MainActivityTest.java
@@ -3,10 +3,16 @@ package com.example.invoify;
 import androidx.test.rule.ActivityTestRule;
 import dev.flutter.plugins.integration_test.FlutterTestRunner;
 import org.junit.Rule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(FlutterTestRunner.class)
 public class MainActivityTest {
     @Rule
     public ActivityTestRule<MainActivity> rule = new ActivityTestRule<>(MainActivity.class, true, false);
+
+    @Test
+    public void test() {
+        // Stub method for JUnit test discovery in CI/Firebase Test Lab
+    }
 }
